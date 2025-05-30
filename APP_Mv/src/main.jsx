a643c863b96f9250602bba4053ed3403fed43a7c
Layssa <layglez@hotmail.com>
2025-05-29 23:31:56 -0600

    fixed main.jsx

--- APP_Mv/src/main.jsx
+++ APP_Mv/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './styles/global.css';
-import './styles/app.css';
+import './styles/App.css';
 
 import App from './App.jsx';
 import PeliculasList from './components/PeliculasList.jsx';
